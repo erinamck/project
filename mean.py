a904cff7622a7b9a8bcf2fcc2a39ad8ceb3f27cc
Erin <erinamck@gmail.com>
2012-11-29 14:27:16 -0500

    Added purpose of program and handled division by zero.

--- mean.py
+++ mean.py
@@ -1,7 +1,12 @@
+#calculate mean for input (list of numbers)
 import sys
 
-sum = 0
-for num in sys.argv[1:]:
-	sum += float(num)
+if len(sys.argv) == 1:
+	print 'Error: no arguments given.'
+	sys.exit()
+else:
+	sum =0
+	for sum in sys.argv[1:]:
+		sum += float(num)
 
-print sum / (len(sys.argv)-1)
+	print sum / (len(sys.argv)-1)
